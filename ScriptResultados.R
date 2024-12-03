2d5284c52f87788436f97bf15306c731aed61fc7
Nico <nicolasmuniz2005@gmail.com>
2024-12-03 19:12:40 +0100

    Commit inicial

---
+++ ScriptResultados.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: 'MR - Trabajo'
+#' author:
+#' - Alicia Losada <alicia.losada.sanchez@udc.es>
+#' - María Cardoso <alicia.losada.sanchez@udc.es>
+#' - Nicolás Muñiz <nicolas.muniz@udc.es>
+#' output: pdf_document
+#' ---
+#' 
+#' **1.** Construcción del modelo.
+#' 
+#' - $E(Y|X) = \beta_0 + \beta_1X_1$
+#' 
