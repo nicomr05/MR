186f269854c2d755c3e43f4d837baedbb5e31931
Nico <nicolasmuniz2005@gmail.com>
2024-12-05 19:44:02 +0100

    Reelección de datos + Inicio de RL + Arreglos en ScriptResultados

--- ScriptResultados.R
+++ ScriptResultados.R
@@ -1,15 +1,32 @@
 #' ---
-#' title: 'MR - Trabajo'
+#' title: '**MR - Trabajo**'
 #' author:
-#' - Alicia Losada <alicia.losada.sanchez@udc.es>
-#' - María Cardoso <alicia.losada.sanchez@udc.es>
-#' - Nicolás Muñiz <nicolas.muniz@udc.es>
+#' - Alicia Losada | <alicia.losada.sanchez@udc.es>
+#' - María Cardoso | <m.cardoso@udc.es>
+#' - Nicolás Muñiz | <nicolas.muniz@udc.es>
+#' date: '**11/12/2024**'
 #' output: pdf_document
 #' ---
+#' $\newline$
 #' 
-#' **1.** Distribución del modelo matemático
+#' **Regresión Lineal Múltiple**
+#' 
+#' **1.** Modelo matemático
 #' 
 #' - $\mathbb{E}[\vec{Y}|\boldsymbol{X}] = \beta_0 + \sum_{i=1}^{n}\beta_iX_{ij}$
 #'
 #'
 #' **2.** Análisis descriptivo
+#' 
+#' 
+#' $\newline$
+#' 
+#' **Regresión Logística**
+#' 
+#' **1.** Modelo matemático
+#'
+#' - $\mathbb{E}[\vec{Y}|X] = \frac{e^{\eta}}{1 + e^{\eta}}$, $\eta = \vec\beta X$
+#' 
+#' 
+#' **2.** Análisis descriptivo
+#' 
